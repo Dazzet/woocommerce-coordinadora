86cb85740e1cc0cca0c94d3626ef880c4642f5bc
Mario Yepes <marioy47@gmail.com>
2018-07-03 18:33:25 -0500

    Saving order data

--- lib/Order.php
+++ lib/Order.php
@@ -25,6 +25,7 @@ class Order
         if (!$this->_initialized) {
             add_action('add_meta_boxes', array($this, 'addMetaboxes'), 10, 2);
             add_action('woocommerce_process_shop_order_meta', array($this, 'processShopOrderMeta'), 10, 2);
+            add_action('save_post', array($this, 'savePost'), 10, 2);
         }
 
     }
@@ -44,27 +45,16 @@ class Order
 
     public function metaboxTracking( $post )
     {
-        $data = get_post_custom($post->ID);
-        if (!isset($data['wc_coordinadora_tracking_code'])) {
-            $data['wc_coordinadora_tracking_code'] = '';
-        }
-        ?>
-<div class="wc-coordinadora-track-infomation">
-<p>
-<label for="wc-coordinadora-tracking-code"><?php _e('Tracking Code', 'wc-coordinadora'); ?></label>
-<input type="text" name="wc_coordinadora_tracking_code" id="wc-coordinadora-tracking-code" placeholder="ABC1234" value="<?php echo $data['wc_coordinadora_tracking_code'] ?>" />
-</p>
-
-</div>
-
-
-        <?php
+        $data = get_post_meta($post->ID, 'wc_coordinadora_tracking_code', true);
+        echo '<p><input type="text" name="wc_coordinadora_tracking_code" placeholder="ABC123" value="'.$data.'"></p>';
     }
 
-    public function processShopOrderMeta($id, $post )
+    public function savePost($id, $post )
     {
-        $order = wc_get_order($id);
-        //wp_die(print_r($order, true));
+        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+            return $id;
+        }
+        update_post_meta($id, 'wc_coordinadora_tracking_code', $_POST['wc_coordinadora_tracking_code']);
     }
 
 
