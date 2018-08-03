d9ce028e4a675337279981fa6a0444f9ffab3e6c
Mario Yepes <marioy47@gmail.com>
2018-08-03 17:41:00 -0500

    Agregando readme

---
+++ README.php
@@ -0,0 +1,16 @@
+# Integración Woocommerce Coordinadora
+
+## Instalación
+
+- `composer update --dev`
+- `coposer update`
+
+## Test
+
+- `cp .env.example .env`
+- Agregue las credenciales a .env
+- `php cli.php`
+
+## Deployment
+
+- `composer rsync`
