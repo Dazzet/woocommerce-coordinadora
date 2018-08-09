b9382cd20ce97df6492ead1d7a479ddd80bb7596
Mario Yepes <marioy47@gmail.com>
2018-08-09 14:43:37 -0500

    Creando testing de Ags

--- test/Webservice/AgsTest.php
+++ test/Webservice/AgsTest.php
@@ -1,26 +1,60 @@
-<?php namespace WcCoordinadora;
+<?php
 
 ini_set("soap.wsdl_cache_enabled", 0);
 
-include __DIR__ . '/vendor/autoload.php';
+use WcCoordinadora\Webservice\Ags;
+use WcCoordinadora\Webservice\RequestParameter;
 
-$dotenv = new \Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+class AgsTest extends PHPUnit\Framework\TestCase
+{
+    public function setUp()
+    {
+        $dotenv = new \Dotenv\Dotenv(__DIR__ . '/../..');
+        $dotenv->load();
 
-$client = new \SoapClient(getenv('WSDL_AGS'), array('trace' => 1));
+        $client = new \SoapClient(getenv('WSDL_AGS'), array('trace' => 1));
 
-$in = Webservice\RequestParameter::instance()
-    ->set('codigo_remision', '8787878')
-    ->set('nit', getenv('NIT'))
-    ->set('div', '')
-    ->set('referencia', '')
-    ->set('imagen', 1)
-    ->set('anexo', 1)
-    ->set('apikey', getenv('APIKEY'))
-    ->set('clave', getenv('CLAVE'));
+        $this->para = RequestParameter::instance()
+            ->set('nit', getenv('NIT'))
+            ->set('imagen', 1)
+            ->set('anexo', 1)
+            ->set('apikey', getenv('APIKEY'))
+            ->set('clave', getenv('CLAVE'));
 
+        $this->ags = Ags::instance($client)->start();
+    }
 
-Webservice\Ags::instance($client)
-    ->start()
-    ->exe('seguimiento', $in)
-    ->printR();
+    public function testDepartamentosReturnsArrayOfObjects()
+    {
+        $res = $this->ags->exe('departamentos');
+        $this->assertInstanceOf('stdClass', $res->result);
+        $this->assertObjectHasAttribute('Cotizador_departamentosResult', $res->result);
+    }
+
+    public function testDepartamentosReturnsAntioquia()
+    {
+        $res = $this->ags->exe('departamentos');
+
+        // Buscamos un objeto que tenga el item antioquia
+        $out = array_reduce($res->result->Cotizador_departamentosResult->item, function($carry, $item){
+            return $carry || ($item->nombre == 'Antioquia');
+
+        }, false);
+
+        $this->assertTrue($out);
+    }
+
+    public function testCiudadesReturnsMedellin()
+    {
+        $res = $this->ags->exe('ciudades');
+
+        // Buscamos un objeto que tenga el item Ciudades
+        $out = array_reduce($res->result->Cotizador_ciudadesResult->item, function($carry, $item){
+            return $carry || ($item->nombre == 'MEDELLIN (ANT)');
+
+        }, false);
+
+        $this->assertTrue($out);
+    }
+}
+
